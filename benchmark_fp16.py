9d52901cbb07d42a6382ed6f211e9b1f191d64be
SeanNaren <seannaren@hotmail.com>
2017-05-06 11:10:27 +0100

    Fixed formatting

--- benchmark_fp16.py
+++ benchmark_fp16.py
@@ -1,12 +1,13 @@
 # This script based on the parameters will benchmark the speed of F16 precision to F32 precision on GPU
 import argparse
 import time
+
 import torch
 from torch.autograd import Variable
 from warpctc_pytorch import CTCLoss
 
-from model import DeepSpeech
 from data.utils import update_progress
+from model import DeepSpeech
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', type=int, default=32)
@@ -49,10 +50,10 @@ def iteration(input_data, cuda_half=False):
     loss = criterion(out, targets, sizes, target_sizes)
     loss = loss / inputs.size(0)  # average the loss by minibatch
     if cuda_half:
-	out = out.cuda().half()
+        out = out.cuda().half()
         loss = loss.cuda().half()
-	out.data = out.data.cuda().half()
-	loss.data = loss.data.cuda().half()
+        out.data = out.data.cuda().half()
+        loss.data = loss.data.cuda().half()
     bwd_time = time.time()
     # compute gradient
     optimizer.zero_grad()
@@ -67,7 +68,7 @@ def iteration(input_data, cuda_half=False):
 def run_benchmark(input_data, cuda_half=False):
     for n in range(args.dry_runs):
         iteration(input_data, cuda_half)
-	update_progress(n / (float(args.dry_runs) - 1))
+        update_progress(n / (float(args.dry_runs) - 1))
     print('\nDry runs finished, running benchmark')
     running_time = 0
     total_fwd_time = 0
@@ -75,26 +76,25 @@ def run_benchmark(input_data, cuda_half=False):
     for n in range(args.runs):
         start, end, fwd_time, bwd_time = iteration(input_data, cuda_half)
         running_time += end - start
-	total_fwd_time += fwd_time
-	total_bwd_time += bwd_time
-	update_progress(n / (float(args.runs) - 1))
+        total_fwd_time += fwd_time
+        total_bwd_time += bwd_time
+        update_progress(n / (float(args.runs) - 1))
     bwd_time = total_bwd_time / float(args.runs)
     fwd_time = total_fwd_time / float(args.runs)
     return running_time / float(args.runs), fwd_time, bwd_time
 
+
 print("Running standard benchmark")
-run_time, fwd_time, bwd_time  = run_benchmark(input_standard)
+run_time, fwd_time, bwd_time = run_benchmark(input_standard)
 input_half = input_standard.cuda().half()
 model = model.cuda().half()
 optimizer = torch.optim.SGD(model.parameters(), lr=3e-4,
-                              momentum=0.9, nesterov=True)
+                            momentum=0.9, nesterov=True)
 print("\nRunning half precision benchmark")
-run_time_half, fwd_time_half, bwd_time_half  = run_benchmark(input_half, cuda_half=True)
+run_time_half, fwd_time_half, bwd_time_half = run_benchmark(input_half, cuda_half=True)
 
 print('\n')
 print("Average times for DeepSpeech training in seconds: ")
 print("F32 precision: Average training loop %.2fs Forward: %.2fs Backward: %.2fs" % (run_time, fwd_time, bwd_time))
-print("F16 precision: Average training loop %.2fs Forward: %.2fs Backward: %.2fs" % (run_time_half, fwd_time_half, bwd_time_half))
-
-
-
+print("F16 precision: Average training loop %.2fs Forward: %.2fs Backward: %.2fs" % (
+    run_time_half, fwd_time_half, bwd_time_half))
